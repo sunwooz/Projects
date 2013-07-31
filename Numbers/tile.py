4366309d4c1c39ceb64f1655a7d2ad4165ba16bd
Daniel Yang <yangsunwoo@gmail.com>
2013-07-30 23:40:26 -0400

    added tile.py

--- Numbers/tile.py
+++ Numbers/tile.py
@@ -3,9 +3,16 @@
 # plan of width and height, using a cost entered by the user.
 
 # Use input as the input can be integer and float
-cost = input("What's the cost per sq. feet? ")
-width = input("What's the width of the floor? ")
-height = input("What's the height of the floor? ")
+import math
 
-print "The total cost is $%.2f for %.2f square feet" \
-      % (width * height * cost, width * height)
+while True:
+
+	width = input("Width of floor in feet: ")
+	height = input("Height of floor in feet: ")
+
+
+	cost_of_floor_per_square_foot = input("Cost of floor per square foot: ")
+
+	square_feet = width * height
+
+	print cost_of_floor_per_square_foot * square_feet
